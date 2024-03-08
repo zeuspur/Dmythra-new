587a2c83284aef0aedfc0427bfe06536b9b74063
amalansab <amalansab777@gmail.com>
2024-03-08 16:01:30 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,9 @@
-import 'package:dmythra2/about_organization.dart';
-import 'package:dmythra2/academic.dart';
-import 'package:dmythra2/controller/splash_screen.dart';
-import 'package:dmythra2/firebase_options.dart';
-import 'package:dmythra2/org_acceptedrequest.dart';
-import 'package:dmythra2/activitypost.dart';
-import 'package:dmythra2/career.dart';
-import 'package:dmythra2/clothes.dart';
-import 'package:dmythra2/counselling.dart';
-import 'package:dmythra2/org_userlist.dart';
-import 'package:dmythra2/org_viewactivity.dart';
-import 'package:dmythra2/organization_sponserlist.dart';
-import 'package:dmythra2/food.dart';
-import 'package:dmythra2/fund.dart';
-import 'package:dmythra2/help.dart';
 import 'package:dmythra2/loginpage.dart';
-import 'package:dmythra2/manageacademic.dart';
-import 'package:dmythra2/managecareer&academic.dart';
-import 'package:dmythra2/managecareer.dart';
-import 'package:dmythra2/managetherapy.dart';
-import 'package:dmythra2/medication.dart';
-import 'package:dmythra2/medicines.dart';
-import 'package:dmythra2/organizationlogin.dart';
-import 'package:dmythra2/orghome.dart';
-import 'package:dmythra2/other.dart';
-import 'package:dmythra2/physiotherapy.dart';
-import 'package:dmythra2/sponserhome.dart';
-import 'package:dmythra2/sponserlogin.dart';
-import 'package:dmythra2/sponserregistration.dart';
-import 'package:dmythra2/sponsor_profile.dart';
-import 'package:dmythra2/sponsor_viewactivities.dart';
 import 'package:dmythra2/startingpage.dart';
-import 'package:dmythra2/therapysupport.dart';
-import 'package:dmythra2/training.dart';
-import 'package:dmythra2/transportation.dart';
-import 'package:dmythra2/user_forgetpassword.dart';
-import 'package:dmythra2/user_sponsorlist.dart';
-import 'package:dmythra2/userhome.dart';
 import 'package:dmythra2/userlogin.dart';
-import 'package:dmythra2/userprofile.dart';
-import 'package:dmythra2/userregistration.dart';
-import 'package:dmythra2/orgviewrequest.dart';
-import 'package:dmythra2/yoga.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main()async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
@@ -80,7 +35,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false ,
-      home: SplashScreenPage(),
+      home: UserLogin(),
     );
   }
 }
